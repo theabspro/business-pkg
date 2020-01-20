ac4e78469a4591ed297730181122fee86be2dd63
Abdul <abdul@uitoux.in>
2020-01-20 22:41:58 +0530

    migra

---
+++ src/database/migrations/2020_01_20_211352_sbu_c.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SbuC extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('sbus', function (Blueprint $table) {
+			$table->increments('id');
+			$table->unsignedInteger('lob_id');
+			$table->string('name');
+			$table->unsignedInteger('created_by_id')->nullable();
+			$table->unsignedInteger('updated_by_id')->nullable();
+			$table->unsignedInteger('deleted_by_id')->nullable();
+			$table->timestamps();
+			$table->softdeletes();
+
+			$table->foreign('lob_id')->references('id')->on('lobs')->onDelete('CASCADE')->onUpdate('cascade');
+
+			$table->foreign('created_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+			$table->foreign('updated_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+			$table->foreign('deleted_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+
+			$table->unique(["lob_id", "name"]);
+		});
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('sbus');
+	}
+}

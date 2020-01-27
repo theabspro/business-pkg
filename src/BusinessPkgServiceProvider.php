163ae65fc434c79e1f274e781bfdbea39789ab42
Abdul <abdul@uitoux.in>
2020-01-27 11:42:27 +0530

    u

--- src/BusinessPkgServiceProvider.php
+++ src/BusinessPkgServiceProvider.php
@@ -17,6 +17,8 @@ class BusinessPkgServiceProvider extends ServiceProvider {
 		$this->loadViewsFrom(__DIR__ . '/views', 'business-pkg');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
+			__DIR__ . '/database/seeds/client' => 'database/seeds',
+			__DIR__ . '/config/config.php' => config_path('business-pkg.php'),
 		]);
 	}
 
